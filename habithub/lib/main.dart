6f0e7b1ceec8cb53636bdaf227575800256a2ffa
Alanthomas-in <alan.thomas.in@gmail.com>
2023-11-13 21:41:58 +0530

    add option added

--- habithub/lib/main.dart
+++ habithub/lib/main.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+class Habit {
+  Habit({
+    required this.name,
+    required this.description,
+    required this.time,
+    required this.isShared,
+  });
+
+  final String name;
+  final String description;
+  final String time;
+  final bool isShared;
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -33,6 +46,9 @@ class _MyHomePageState extends State<MyHomePage> {
     Text('Shared Habits'),
     Text('Account'),
   ];
+
+  // Define the habits list
+  List<Habit> habits = [];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -82,11 +98,80 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Add your onPressed code here!
+          TextEditingController habitNameController = TextEditingController();
+          TextEditingController descriptionController = TextEditingController();
+          TextEditingController timeController = TextEditingController();
+          bool isShared = false;
+
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Add a new habit'),
+                content: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: habitNameController,
+                      decoration: InputDecoration(hintText: "Habit name"),
+                    ),
+                    TextField(
+                      controller: descriptionController,
+                      decoration: InputDecoration(hintText: "Description"),
+                    ),
+                    TextField(
+                      controller: timeController,
+                      decoration: InputDecoration(hintText: "Time for notification"),
+                    ),
+                    CheckboxListTile(
+                      title: Text("Shared"),
+                      value: isShared,
+                      onChanged: (newValue) {
+                        setState(() {
+                          isShared = newValue ?? false;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Add'),
+                    onPressed: () {
+                      // Add your habit addition code here
+                      String habitName = habitNameController.text;
+                      String description = descriptionController.text;
+                      String time = timeController.text;
+
+                      // Create a new habit with the provided details
+                      Habit newHabit = Habit(
+                        name: habitName,
+                        description: description,
+                        time: time,
+                        isShared: isShared,
+                      );
+
+                      // Add the new habit to your list of habits
+                      habits.add(newHabit);
+
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
         },
         child: const Icon(Icons.add),
         backgroundColor: Colors.deepPurple,
       ),
+
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
