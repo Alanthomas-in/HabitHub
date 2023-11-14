0d68d0071f6a6971504615693303739c71b279f0
Alanthomas-in <alan.thomas.in@gmail.com>
2023-11-14 06:45:57 +0530

    improved UI

--- habithub/lib/LoginPage.dart
+++ habithub/lib/LoginPage.dart
@@ -16,52 +16,77 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Login'),
-      // ),
-      body: _isLoggedIn ? MyHomePage() : Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Enter your username'),
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return 'Please enter your username';
-                }
-                return null;
-              },
+      appBar: AppBar(
+        title: Text('Habit Hub'),
+        centerTitle: true,
+      ),
+      body: _isLoggedIn ? MyHomePage() : Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter your username',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter your username';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16.0), // Add space between the fields
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter your password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16.0), // Add space between the fields
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple, // background color
+                    onPrimary: Colors.white, // text color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      setState(() {
+                        _isLoggedIn = true;
+                      });
+                    }
+                  },
+                  child: Text('Login'),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.deepPurple, // text color
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => CreateUserPage()),
+                    );
+                  },
+                  child: Text('Create New User'),
+                ),
+              ],
             ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Enter your password'),
-              obscureText: true,
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return 'Please enter your password';
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState?.validate() ?? false) {
-                  setState(() {
-                    _isLoggedIn = true;
-                  });
-                }
-              },
-              child: Text('Login'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CreateUserPage()),
-                );
-              },
-              child: Text('Create New User'),
-            ),
-          ],
+          ),
         ),
       ),
     );
