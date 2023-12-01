579cebca41b16bcd0cc961491922c2b06e2f46e9
Alanthomas-in <alan.thomas.in@gmail.com>
2023-12-01 11:06:54 +0530

    Connected Firebase to the app

--- habithub/lib/main.dart
+++ habithub/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:habithub/firebase_options.dart';
 import 'LoginPage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
